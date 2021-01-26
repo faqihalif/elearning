350ecda4b013a2dada77301130b1e70b1c66e3c6
faqihalif <faqihalif@live.com>
2021-01-26 17:00:48 +0700

    .

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import ImageBanner from '../images/banner.jpg'
 
 function Banner() {
     return (
-        <div className="container px-4 py-4 mx-auto lg:px-8">
-            <div className="w-auto bg-gray-700 rounded-2xl" style={{height: "520px"}}>
-            </div>
+        <div className="container py-4 mx-auto lg:px-8">
+            <img src={ImageBanner} alt="Banner E-Learning JEC" className="rounded-xl" />
         </div>
     )
 }
