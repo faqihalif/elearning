64a110b5b24143ab14c05daf851927e0048a8e3f
faqihalif <faqihalif@live.com>
2021-01-26 17:21:29 +0700

    .

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import ImageBanner from '../images/banner.jpg'
 
 function Banner() {
     return (
-        <div className="container py-4 mx-auto lg:px-8">
+        <div className="container py-4 mx-auto">
             <img src={ImageBanner} alt="Banner E-Learning JEC" className="rounded-xl" />
         </div>
     )
