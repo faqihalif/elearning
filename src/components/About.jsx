5fed5a0cb4df44475472223bf4f62316107ed1f4
Faqih Alif <faqihalif@live.com>
2021-01-28 14:01:56 +0700

    .

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ function About() {
                             <p className="mb-2 text-2xl font-extrabold text-gray-800 lg:mb-6 lg:text-4xl">Find Your Course That Makes Bright Future</p>
                             <div className="w-32 h-1 bg-blue-500 rounded-full lg:h-2"></div>
                         </div>
-                        <p className="mt-2 leading-loose text-gray-500 lg:mt-6 lg:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae illo debitis dolor nemo nisi exercitationem, corporis natus, magni enim necessitatibus voluptatem ex voluptatum repudiandae quibusdam, officia dignissimos nihil temporibus vero?</p>
+                        <p className="mt-2 leading-loose text-gray-500 lg:mt-6 lg:text-lg">E-Learning JEC is a Learning Management System with interactive learning methods that can help you to take your skills to the next level 💪💪💪</p>
                     </div>
                     <div>
                         <img src={ImageVisual} alt="" className="w-full h-full" />
