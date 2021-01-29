a826ea75b3237e13ce3e1995694c4a09cca9fc97
Faqih Alif <faqihalif@live.com>
2021-01-29 14:06:00 +0700

    .

--- src/components/EnrollNow.jsx
+++ src/components/EnrollNow.jsx
@@ -4,10 +4,10 @@ function EnrollNow() {
     return (
         <div className="enroll-bg">
             <div className="container px-4 py-12 mx-auto lg:py-24 md:px-8">
-                <p className="mb-6 text-2xl font-extrabold text-center text-gray-800 lg:text-4xl">Lets Begin An Online Course</p>
+                <p className="mb-6 text-2xl font-extrabold text-center text-gray-800 lg:text-4xl">Catch Our Various Course Now!!</p>
                 <div className="flex justify-center">
                     <a href="https://elearning.jec.co.id/" className="inline-flex px-4 py-2 text-sm font-bold text-white bg-blue-500 shadow-lg lg:text-base lg:px-6 lg:py-3 rounded-xl">
-                        Catch Our Various Course Now!!
+                        ENROLL NOW
                     </a>
                 </div>
             </div>
